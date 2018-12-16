72373c6d54ec2b8526c160bb70e115c44f44e9d1
fxq666 <2406843325@qq.com>
2018-12-16 15:46:34 +0800

    上传项目

---
+++ demo/test.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+public class test {
+    public static void main(String[] args) {
+        Pig pig1 = new Pig();
+
+        Pig pig2 = new Pig();
+
+        System.out.println(pig1==pig2);
+        System.out.println(pig1.equals(pig2));
+
+        System.out.println(pig1.getClass());
+        System.out.println(pig2.getClass());
+    }
+}
